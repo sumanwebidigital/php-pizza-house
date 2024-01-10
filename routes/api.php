f34b81de245f5e48c10423cc57cefd67597066c5
Suman Neupane <suman.neupane@webo.digital>
2024-01-10 08:51:52 +0545

    Working on creating save pizza api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PizzaApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('pizzas', [PizzaApiController::class, 'getPizzas']);
+Route::get('pizzas/{id}', [PizzaApiController::class, 'getPizzaById']);
+Route::post('pizzas', [PizzaApiController::class, 'insert']);
